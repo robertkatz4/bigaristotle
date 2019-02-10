b9b0afed7ae03a817a1eb84fb6a6e506ec926b1f
robertkatz4 <robert.katz@successacademies.org>
2019-02-10 13:37:20 -0500

    initial dynamodb commit

---
+++ exportdynamodb.py
@@ -0,0 +1,39 @@
+import os
+import boto3
+
+session = boto3.session.Session(profile_name=os.environ["AWS_PROFILE"])
+boto3.session.Session().available_profiles
+dynamodb = session.resource('dynamodb', region_name='us-east-1', endpoint_url="https://dynamodb.us-east-1.amazonaws.com:443")
+
+
+table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
+
